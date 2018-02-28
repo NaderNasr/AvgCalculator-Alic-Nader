019c199a4911a6697ac90a0aa8a26cc402721580
Nader Nasr <nadernasr7@gmail.com>
2018-02-28 16:30:56 +0200

    Blueprint

---
+++ main.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+
+  var counter = counter++;
+
+
+  $('body').css({'background-color': '#425cf4'})
+  $("#b1").click(function(){
+  var text=$("<label>subject: </label><input type='textbox' id='t1' > <br>")
+  text.appendTo("body")
+  var text=$("<label>subject: </label><input type='textbox' id='t1' > <br>")
+  text.appendTo("body")
+
+})
+
+
+
+});
