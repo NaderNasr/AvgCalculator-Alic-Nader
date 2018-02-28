37d6474ce8b5550ef9ea5d3e4afc100a29a4e6a0
Nader Nasr <nadernasr7@gmail.com>
2018-03-01 01:22:09 +0200

    beta v3

--- myscripty.js
+++ myscripty.js
@@ -1,43 +1,40 @@
-$(document).ready(function(){
+  $(document).ready(function(){
 
 
- var sum=0
+    var sum=0
 
-     $("#b1").click(function(){
-     	
-var m=parseInt(($("#"+num).val()))
-     	conuter();
-     var text = $("<label> subject # "+num+" "+" "+" </label><input type='text' id='"+num+"' > <br>")
-     
+    $("#b1").click(function(){
+
+      var m=parseInt(($("#"+num).val()))
+      conuter();
+      var text = $("<label> Grade # "+num+" "+" "+" </label><input type='text' id='"+num+"' > <br></br>")
+      var text2 = $("<label> Subject "+num+" "+" "+" </label><input type='text' id='"+num+"' > <br></br>").style('position': 'center')
       sum=sum+m
+      text.appendTo("body");
+      text2.appendTo("body");
 
-   
-     text.appendTo("body");
-    
-});
-     $("#res").click(function(){
+    });
+      $("#res").click(function(){
 
-     	alert(sum/num)
-       text.appendTo("body");
-     });
+        alert(sum/num)
+        text.appendTo("body");
+      });
 
 
-var num=0
-function conuter(){
-	num++
-
- if(num>10)
-     {
-     	alert("10 JUST YA BABY :*");
-       return
-     }
-     return num
-}
+        var num=0
+        function conuter(){
+	      num++
+        if(num>15){
+        alert("15 Subjects Allowed");
+        return
+        }
+        return num
+        }
 
 
 
 
- 
+
 
 
 
